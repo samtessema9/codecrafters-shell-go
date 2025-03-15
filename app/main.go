8a5e0da9780a13e81f95ee5219661635b279f3b2
Sam Tessema <samtessema@Sams-MacBook-Air.local>
2025-03-15 11:51:40 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,14 +12,16 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Errorf("Error reading input: %v", err)
+	for {
+		// Prompt User
+		fmt.Fprint(os.Stdout, "$ ")
+	
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Errorf("Error reading input: %v", err)
+		}
+	
+		fmt.Printf("%v: command not found\n", strings.TrimSpace(command))
 	}
-
-	fmt.Printf("%v: command not found", strings.TrimSpace(command))
 }
